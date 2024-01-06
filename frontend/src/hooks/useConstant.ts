5b4b46a1365fd1c7241d76dfd24a2a3e784775fd
KJK <kody.j.king@gmail.com>
2024-01-06 14:51:44 -0800

    typo

--- frontend/src/hooks/useConstant.ts
+++ frontend/src/hooks/useConstant.ts
@@ -1,6 +1,6 @@
 import { useRef } from "react"
 
-// Guarentees that the initialization function passed in is only called once.
+// Guarantees that the initialization function passed in is only called once.
 export function useConstant<T>( fn: () => T ): T {
     const ref = useRef<T | null>( null )
     if ( ref.current === null )
